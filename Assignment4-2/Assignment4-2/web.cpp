a4c452283b42238b786f3d82e33889fcccdff7e7
Min-Hyeok Seong <sminhyuck@naver.com>
2019-04-02 10:24:46 +0900

    Update

--- Assignment4-2/Assignment4-2/web.cpp
+++ Assignment4-2/Assignment4-2/web.cpp
@@ -9,22 +9,21 @@ using namespace std;
 struct log {
 	string IP, Time, URL, Status;	// IP, 시간 연 월 일 시, URL, Status
 }typedef LOG;
+vector<LOG> csv;
 
-string month[12] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 // IP부터 Status순 정렬까지
 bool compare1(const LOG& x, const LOG& y) { return x.IP < y.IP; }
 bool compare2(const LOG& x, const LOG& y) { return x.Time < y.Time; }
 bool compare3(const LOG& x, const LOG& y) { return x.URL < y.URL; }
 bool compare4(const LOG& x, const LOG& y) { return x.Status < y.Status; }
 
-int index[4] = { 0,1,2,3 };	// 정렬한 후, 표기할 순서 
-// print시 참조
+int index[4] = { 0,1,2,3 };	// 정렬한 후, 표기할 순서 Print에 이용
 string index2[4] = { "    IP: ","    Time: ", "    URL: ", "    Status: " };
-vector<LOG> csv;
+string month[12] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
 
 void process();
 void change(int n);		// 0은 정렬을 위해  연/월/일 순으로 변환, 1은 정렬 후 일/월/연 순으로 복원
-
 void read(string file);
 void sort(string type);
 void print();
@@ -56,7 +55,7 @@ void process() {
 
 
 }
-// 30/Nov/2017:14:27:53
+
 void change(int n) {
 	if (n == 0) {
 		for (int i = 0; i < csv.size(); i++) {
@@ -104,18 +103,20 @@ void read(string file) {
 		temp.Status = parse;
 		csv.push_back(temp);
 	}// csv 벡터에 , 로 구분하여 csv데이터 저장
-	change(0);
 }
+
 void sort(string type) {	// 정렬 방식, 출력 순서 선택
 	if (type == "-ip") { sort(csv.begin(), csv.end(), compare1); index[0] = 0; index[1] = 1; index[2] = 2; index[3] = 3; }
 	else if (type == "-t") { 
+		change(0);	// 정렬 위해 순서변환
 		sort(csv.begin(), csv.end(), compare2); 
-		change(1);
+		change(1);	// 출력 위해 순서복원
 		index[0] = 1; index[1] = 0; index[2] = 2; index[3] = 3; 
 	}
 	else if (type == "-u") { sort(csv.begin(), csv.end(), compare3); index[0] = 2; index[1] = 0; index[2] = 1; index[3] = 3; }
 	else if (type == "s") { sort(csv.begin(), csv.end(), compare4); index[0] = 3; index[1] = 0; index[2] = 1; index[3] = 2; }
 }
+
 void print() {
 	for (int i = 0; i < csv.size(); i++) {
 		string temp[4];
